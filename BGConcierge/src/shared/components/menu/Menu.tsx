0630d31cfe1bb8c41ab296c20c7274a7c548d126
Jose Candido <jcqlfh@gmail.com>
2023-02-12 15:46:18 -0300

    fixing menu

--- BGConcierge/src/shared/components/menu/Menu.tsx
+++ BGConcierge/src/shared/components/menu/Menu.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Appbar } from 'react-native-paper';
+
 function Menu(): JSX.Element {
 
   return (
-    <Appbar mode='center-aligned' elevated={true}> 
-      <Appbar.Action icon={require('@assets/logo.png')} size={64}/>
-      <Appbar.Content title="BGConcierge" />
+    <Appbar mode='center-aligned' style={{ backgroundColor: '#04FD9420'}}> 
+      <Appbar.Action icon={require('@assets/images/logo.png')} size={64}/>
+      <Appbar.Content title="BGConcierge"  titleStyle={{fontFamily: 'BigelowRules', lineHeight: 48, letterSpacing: 1, fontSize: 48, margin: 10, textAlign:'center'}}/>
     </Appbar>
   );
 }
