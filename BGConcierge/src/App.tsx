8a4af42eb50a13aaad706791146410c2c4a63cae
Jose Candido <jcqlfh@gmail.com>
2023-02-13 14:51:50 -0300

    Fixing collection

--- BGConcierge/src/App.tsx
+++ BGConcierge/src/App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Text} from 'react-native-paper';
 import { FlatList, ScrollView, View } from 'react-native';
 import Header from '@components/header/Header';
@@ -13,18 +13,52 @@ import Title from '@components/title/Title';
 import Collection from '@components/collection/Collection';
 
 function App(): JSX.Element {
+  const [data, setData] = useState([
+    {name: 'BGG Ranking', isEditable: false},
+    {name: 'jcqlfh', isEditable: true},
+  ]);
+  const [rerender, setRerender] = useState(new Date());
+  const [selectedItem, setSelectedItem] = useState('');
+
+  console.log(selectedItem);
+
   return (
     <View style={{flex: 1}}>
       <Header />
       <Title text={'Choose a Collection'} />
-      <Button icon='plus' style={{margin:10}}buttonColor='blue'>Add Collection</Button>
+      <Button icon='plus' style={{margin:10}}buttonColor='#4504FD20' onPress={() => {setData([{name: '', isEditable: true}, ...data]); setRerender(new Date())}}>Add Collection</Button>
       <View style={{flex: 1}}>
-      <FlatList data={[
-          {name: 'BGG Ranking', isEditable: false},
-          {name: 'jcqlfh', isEditable: true},
-        ]}
+      <FlatList data={data}
+        extraData={rerender}
         renderItem={
-          ({item}) => <Collection name={item.name} isEditable={item.isEditable} />
+          ({item, index}) => 
+          <Collection 
+            name={item.name}
+            isEditable={item.isEditable} 
+            isSelected={selectedItem === item.name} 
+            onChange={
+              (newItem) => 
+              {
+                data[index].name = newItem
+                setData(data);
+                setRerender(new Date());
+              }
+            }
+            onSelected={
+              (name: string) => 
+              {
+                setSelectedItem(name);
+                setRerender(new Date());
+              }
+            }
+            onDelete={
+              () =>
+              {
+                data.splice(index, 1);
+                setData(data);
+                setRerender(new Date());
+              }
+            }/>
         }
       />
       </View>
